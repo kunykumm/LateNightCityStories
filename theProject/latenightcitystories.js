e18c187e17764bb39585a95999d90b4aa2b92705
kunykumm <55748680+kunykumm@users.noreply.github.com>
2020-12-04 21:44:27 +0100

    Html page almost working

---
+++ theProject/latenightcitystories.js
@@ -0,0 +1,23 @@
+var title;
+var moodText;
+var warmButton;
+var coldButton;
+var description;
+var generateButton;
+
+function setup() {
+  setupWindow();
+}
+
+function setupWindow() {
+  canvas = createCanvas(600, 600);
+  canvas.position(260, 50);
+  background(0);
+}
+
+function getWords(theForm) {
+  //print("I got here!");
+}
+  
+function draw() {
+}
