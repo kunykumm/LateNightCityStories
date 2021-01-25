7e9224d0154f13e9dbdaca17c29a4d018d83c488
kunykumm <55748680+kunykumm@users.noreply.github.com>
2021-01-25 23:52:33 +0100

    Finally some progress.

--- theProject/latenightcitystories.js
+++ theProject/latenightcitystories.js
@@ -21,6 +21,14 @@ var secondEmotions;
 var thirdEmotions;
 
 var generate;
+var pOne;
+var pTwo;
+var pThree;
+
+var pOneCounter;
+var xCity;
+var yCity;
+var timeBezier;
 
 function preload() {
   jsonEndIndices = [966, 1682, 3112, 4052, 4689, 5311, 5732, 6199, 6922, 7028, 7103, 7568, 8268,
@@ -38,6 +46,11 @@ function preload() {
 function setup() {
   setupWindow();
   generate = false;
+  pOne = false;
+  pTwo = false;
+  pThree = false;
+  pOneCounter = 0;
+  timeBezier = 0;
 }
 
 function setupWindow() {
@@ -68,23 +81,19 @@ function getWords() {
   }
 
   validateWords();
+  calculateCityCentre();
 
   generate = true;
+  pOne = true;
 }
 
 function validateWords() {
-  validateAWord(firstWord, firstEmotions);
-  validateAWord(secondWord, secondEmotions);
-  validateAWord(thirdWord, thirdEmotions);
-  print(firstWord);
-  print(firstEmotions);
-  print(secondWord);
-  print(secondEmotions);
-  print(thirdWord);
-  print(thirdEmotions);
+  firstEmotions = validateAWord(firstWord);
+  secondEmotions = validateAWord(secondWord);
+  thirdEmotions = validateAWord(thirdWord);
 }
 
-function validateAWord(word, emotions) {
+function validateAWord(word) {
   print("WordValidation START");
   word = word.toLowerCase();
   var index = findFirstLetterIndexRange(word);
@@ -99,8 +108,7 @@ function validateAWord(word, emotions) {
   stop = jsonEndIndices[index];
 
   var wordIndex = findWordInJson(word, start, stop);
-  findTheEmotions(emotions, wordIndex);
-  print("WordValidation END")
+  return findTheEmotions(wordIndex);
 }
 
 function findFirstLetterIndexRange(word) {
@@ -122,27 +130,131 @@ function findWordInJson(word, start, stop) {
   return -1;
 }
 
-function findTheEmotions(emotions, wordIndex) {
-  emotions = [
-              jsonData[wordIndex].Positive,
-              jsonData[wordIndex].Negative,
-              jsonData[wordIndex].Anger,
-              jsonData[wordIndex].Anticipation,
-              jsonData[wordIndex].Disgust,
-              jsonData[wordIndex].Fear,
-              jsonData[wordIndex].Joy,
-              jsonData[wordIndex].Sadness,
-              jsonData[wordIndex].Surprise,
-              jsonData[wordIndex].Trust
-              ]
+function findTheEmotions(wordIndex) {
+  if (wordIndex == -1) {
+    emotions = ['1', '0', '0', '0', '0', '0', '1', '0', '0', '0'];
+  } else {
+    emotions = [
+                jsonData[wordIndex].Positive,
+                jsonData[wordIndex].Negative,
+                jsonData[wordIndex].Anger,
+                jsonData[wordIndex].Anticipation,
+                jsonData[wordIndex].Disgust,
+                jsonData[wordIndex].Fear,
+                jsonData[wordIndex].Joy,
+                jsonData[wordIndex].Sadness,
+                jsonData[wordIndex].Surprise,
+                jsonData[wordIndex].Trust
+                ]
+  }
+  print(emotions);
   print("Emotions filled.");
+  return emotions;
 }
   
 function draw() {
   if (!generate) return;
   
-  fill(random(50, 255));
-  circle(400, 200, random(50, 150));
+  if (pOne) {
+    print("phaseOne");
+    phaseOne();
+  }
 
-  generate = false;
+  if (pTwo) {
+    generateCentreNoise();
+  }
+
+  if (pThree) {
+    
+    generate = false;
+  }
+}
+
+function phaseOne() {
+  //calculateCityCentre();
+  generateNoise();
+  //generateCentreNoise();
+  pOneCounter++;
+  if (pOneCounter == 10) {
+    pOne = false;
+    pTwo = true;
+  }
+}
+
+function calculateCityCentre() {
+  for (i = 0; i < 5; ++i) {
+    xCity = xCity + firstEmotions[i] + secondEmotions[i] + thirdEmotions[i];
+    yCity = yCity + firstEmotions[i + 5] + secondEmotions[i + 5] + thirdEmotions[i + 5];
+  }
+  xCity = xCity / 3;
+  yCity = yCity / 3;
+}
+
+function generateNoise() {
+  
+  var allowF = false;
+  print("noise");
+  
+  stroke(255, random(150, 170), 26);
+  fill(0);
+
+  n_range = 10;
+  step = 4;
+  treshold = .98;
+
+  fillBackground = 0;
+
+  num = firstEmotions[pOneCounter] + secondEmotions[pOneCounter] + thirdEmotions[pOneCounter];
+  if (num > 0) {
+    n_range = random(10, 40);
+    fillBackground = 80;
+  }
+
+  for (x = 0; x < width; x += step) {
+    for (y = 0; y < height; y += step) {
+      
+      nx = map(x, 0, width, 0, n_range);
+      ny = map(y, 0, height,0, n_range);
+
+      n_value = noise(nx, ny) * (220 - fillBackground);
+      
+      if (n_value  < 100 && random(1) > treshold) {
+        strokeWeight(random(0.4));
+        square(x + random(2), y + random(2), random(3, 5));
+      }
+    }
+  }
+
+  noStroke();
+  fill(0, 10);
+  rect(0, 0, width, height);
+}
+
+function generateCentreNoise() {
+
+  stroke(255, random(150, 170), 26);
+  fill(0);
+
+  print("startBezier");
+  var x1 = width * noise(timeBezier + 15);
+  var x2 = width * noise(timeBezier + 25);
+  var x3 = width * noise(timeBezier + 35);
+  var x4 = width * noise(timeBezier + 45);
+
+  var y1 = height * noise(timeBezier + 55);
+  var y2 = height * noise(timeBezier + 65);
+  var y3 = height * noise(timeBezier + 75);
+  var y4 = height * noise(timeBezier + 85);
+
+  print("cycleBezier");
+  let steps = 10;
+  for (let i = 0; i < steps; ++i) {
+    let timeCycle = i / steps * 50;
+    let x = bezierPoint(x1, x2, x3, x4, timeCycle);
+    let y = bezierPoint(y1, y2, y3, y4, timeCycle);
+    square(x, y, random(3, 5));
+  }
+
+  timeBezier += 0.005;
+  print("endBezier");
 }
